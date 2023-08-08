5c50cd9800cedac332feddf480626f519b9c808e
MousElPsyCongroo <mtapha.tall@gmail.com>
2023-08-08 11:26:30 +0200

    1 - Parse headings

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 A Markdown to Html parser made at Holberton School
 
 Usage: ./markdown2html.py <md_file> <html_file>
+
+# My title
+## My title2
+# My title3
+#### My title4
+### My title5
--- markdown2html.py
+++ markdown2html.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Markdown to html parser"""
 from sys import argv, exit
+import re
 
 argslen = len(argv) - 1
 
@@ -10,11 +11,30 @@ def main():
         exit('Usage: ./markdown2html.py README.md README.html')
 
     md_file = argv[1]
+    html_file = argv[2]
     try:
-        with open(md_file, 'r', encoding='utf-8') as f:
+        with open(md_file, 'r', encoding='utf-8') as md:
+            with open(html_file, 'w', encoding='utf-8') as html:
+                while True:
+                    line = md.readline()
+
+                    if not line:
+                        break
+                    converted = convert_line(line)
+                    html.write(converted)
+
             pass
     except FileNotFoundError:
         exit(f'Missing {md_file}')
+
+
+def convert_line(line):
+    """Converts a markdown line to an html one"""
+    if '#' in line:
+        heading_level = line.count('#')
+        line = re.sub('(#+) +', f'<h{heading_level}>', line)
+        line = line.replace('\n', f'</h{heading_level}>\n')
+    return line
 
 
 if __name__ == '__main__':
