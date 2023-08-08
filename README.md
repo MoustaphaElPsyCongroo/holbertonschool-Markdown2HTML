fdf20832d74dc601ce38c867c45ae6a5be6be579
MousElPsyCongroo <mtapha.tall@gmail.com>
2023-08-08 10:00:36 +0200

    0 - Initialize script

---
+++ README.md
@@ -0,0 +1,4 @@
+# Markdown to HTML
+A Markdown to Html parser made at Holberton School
+
+Usage: ./markdown2html.py <md_file>
---
+++ markdown2html.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Markdown to html parser"""
+from sys import argv, exit
+
+argslen = len(argv) - 1
+
+
+def main():
+    if argslen < 2:
+        exit('Usage: ./markdown2html.py README.md')
+
+    md_file = argv[1]
+    try:
+        with open(md_file, 'r', encoding='utf-8') as f:
+            pass
+    except FileNotFoundError:
+        exit(f'Missing {md_file}')
+
+
+if __name__ == '__main__':
+    main()
